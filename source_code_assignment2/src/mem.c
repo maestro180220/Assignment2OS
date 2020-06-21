6e12737beaeab60145b43ca63629b45a44013eae
maestro180220 <huan.tran180220@hcmut.edu.vn>
2020-06-21 18:06:42 +0700

    test

--- source_code_assignment2/src/mem.c
+++ source_code_assignment2/src/mem.c
@@ -49,10 +49,14 @@ static struct page_table_t * get_page_table(
 	 * field of the row is equal to the index
 	 *
 	 * */
-
+	struct page_table_t * page_table;
 	int i;
 	for (i = 0; i < seg_table->size; i++) {
-		seg_table->table[i]
+		if (seg_table->table[i].v_index == index)
+		{
+			return seg_table->table[i].pages;
+		}
+		
 	}
 	return NULL;
 
